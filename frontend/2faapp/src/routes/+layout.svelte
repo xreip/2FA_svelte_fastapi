38428872c2ce89081057fab66affb53e1b99ed98
xreip <pierre.mouffe@gmail.com>
2023-02-25 18:01:30 +0100

    layout_base

--- frontend/2faapp/src/routes/+layout.svelte
+++ frontend/2faapp/src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="app flex flex-col min-h-screen">
+	<main class="flex flex-col flex-1 p-4 container">
+		<slot />
+	</main>
+</div>
--- frontend/2faapp/src/routes/+page.svelte
+++ frontend/2faapp/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<svelte:head>
+	<title>Home - Login</title>
+	<meta name="Login" content="Login screen" />
+</svelte:head>
+
+<section class="flex-[0.8] flex flex-col justify-center items-center">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</section>
