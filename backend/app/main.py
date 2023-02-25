fcc07d695999dc8560d4a7bce8839757819aa675
xreip <pierre.mouffe@gmail.com>
2023-02-25 17:24:44 +0100

    fastapi + svelte-tailwind base setup

---
+++ backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Depends
+# from config.config import initiate_database
+
+
+# from app.api.api import api_router
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# app.include_router(api_router) # ADD THE CENTRAL ROUTER
+
+
+# @app.on_event("startup")
+# async def app_init():
+#     await initiate_database()
